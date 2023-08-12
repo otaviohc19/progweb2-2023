a719c8486573eff66fb6de7fe61b7ace4ae9beb5
Otávio Henrique Cardoso Rodrigues <64485490+otaviohc19@users.noreply.github.com>
2023-08-11 22:15:54 -0300

    Add files via upload

---
+++ Passagens/script.js
@@ -0,0 +1,15 @@
+// // Pegar um Elemento HTML para usá-lo no Javascript
+// const elemento = document.querySelector("#idDoElemento");
+
+// // Criar uma ação em um elemento (como um botão)
+// elemento.addEventListener("click", executarFuncao);
+
+// // Função de execução da ação
+// function executarFuncao() {
+
+// }
+
+const partida = document.querySelector("#cmbPartida");
+const destino = document.querySelector("cmbDestino");
+
+alert("OK")
