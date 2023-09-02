eca5dc07e79612e237215c0f0f9433f15f7a9ad9
Otávio Henrique Cardoso Rodrigues <64485490+otaviohc19@users.noreply.github.com>
2023-09-01 22:16:11 -0300

    Aula 01/09

--- site_serie/site_serie/scripts.js
+++ site_serie/site_serie/scripts.js
@@ -31,6 +31,78 @@ for (let ator of bd.elenco) {
     elencoContainer.insertAdjacentHTML("beforeend", html);
 }
 
+// TEMPORADAS
+let tempContainer = document.querySelector(".temporadas-container");
+tempContainer.innerHTML = "";
+
+for (let temp of bd.temporadas) {
+    let numero = temp.numero;
+    let html = `
+    <div class="temporada">
+        <div class="poster">
+            <img src="${temp.poster}">
+        </div>
+        <div class="info">
+            <h3>Temporada ${numero}</h3>
+            <div class="subtitulo">
+                ${temp.numero_episodios} episódios
+            </div>
+            <p class="sinopse">
+                ${temp.resumo}
+            </p>
+            <a href="#" class="botao" id="btn-${numero}">Ver episódios</a>
+            <div class="episodios" id="episodios-${numero}"></div>
+        </div>
+    </div>
+    `;
+
+    // COLOCA NA PAGINA PRIMEIRO
+    tempContainer.insertAdjacentHTML("beforeend", html);
+    // DEPOIS PEGA O BOTÃO
+    let btnEpisodios = document.querySelector(`#btn-${numero}`);
+    btnEpisodios.addEventListener("click", mostraEpisodios);
+}
+
+function mostraEpisodios(evento) {
+    // Impedindo o comportamento padrão do elemento
+    evento.preventDefault();
+
+    // Pegando o elemento que disparou o evento(clique)
+    let target = evento.target;
+    // Pega o ID dele
+    let id = target.getAttribute("id");
+    // Pega o numero dentro da string || substring(onde começa, onde termina) "btn-1"
+    let numero = parseInt(id.substring(4));
+
+    let epContainer = document.querySelector(`#episodios-${numero}`);
+    
+    // Verificando se já existe conteúdo no container de episódios
+    if (epContainer.innerHTML != "") {
+        epContainer.innerHTML = "";
+        return;
+    }
+
+    for (let episodio of bd.temporadas[numero-1].episodios){
+    const html = `
+        <div class="episodio">
+            <div class="foto">
+                <img src="${episodio.foto}" />
+                <span class="nota"><img src="star.png">${episodio.nota}</span>
+            </div>
+            <div class="info">
+                <h4>${episodio.numero} | ${episodio.nome}</h4>
+                <p>${episodio.resumo}</p>
+            </div>
+        </div>
+    `;
+
+    epContainer.insertAdjacentHTML("beforeend", html);
+    }
+
+}
+
+
+
 /*
 afterbegin -> Depois do comeco
 beforebegin -> Antes do comeco
